a511980e5ab22ea546c55e14db797450de592bf0
Vathsala Achar <vathsala.achar@atheon.co.uk>
2017-05-16 13:40:25 +0100

    Connector to database

---
+++ trext/db/conn.py
@@ -0,0 +1,34 @@
+import pyodbc
+
+
+def _get_connector(dbtype=None):
+    connector = None
+    if not dbtype:
+        connector = pyodbc
+    if dbtype == 'exasol':
+        # todo need to document and extend this
+        connector = __import__('exasol')
+    return connector
+
+
+class AnyDB(object):
+    def __init__(self, connection_string, dbtype=None):
+        self._db_connection_string = connection_string
+        self._db_type = dbtype
+        self._db_connection = None
+        self._db_cursor = None
+
+    def get_cursor(self):
+        connection_library = _get_connector(self._db_type)
+        self._db_connection = connection_library.connect(self._db_connection_string)
+        self._db_cursor = self._db_connection.cursor()
+        return self._db_cursor
+
+    def close(self):
+        self._db_connection.close()
+
+
+if __name__ == '__main__':
+    db_conn = AnyDB("DSN=exaro", dbtype='exasol')
+    cursor = db_conn.get_cursor()
+    db_conn.close()
