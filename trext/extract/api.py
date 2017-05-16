1f6edab840d1ab4e0ef0230468d49391d4ffab11
Vathsala Achar <vathsala.achar@atheon.co.uk>
2017-05-16 15:48:28 +0100

    First pass of the trext api and the tableau server connection

---
+++ trext/extract/api.py
@@ -0,0 +1,64 @@
+from trext.extract.server import Tableau
+import os
+
+
+class Extract(object):
+    def __init__(self, is_temp=True):
+        self._location = None
+        self._is_temp = is_temp
+
+    def create(self, view_or_table_name, conn_string, dbtype=None):
+        """        
+        Create an extract
+        >>> import trext
+        >>> tde = trext.Extract()
+        >>> connection_string = "appropriate db connection string"
+        >>> tde.create("db.schema.table", conn_string=connection_string)
+        Created!
+        
+        :param view_or_table_name: 
+        :param conn_string: 
+        :param dbtype: 
+        :return: 
+        """
+        # todo
+        self._location = 'path_to_tde'
+
+    @property
+    def location(self):
+        """
+        >>> tde.location
+        /temp/extract.tde
+        
+        :return: 
+        """
+        return self._location
+
+    @location.setter
+    def location(self, tde_path):
+        self._location = tde_path
+
+    def publish(self, host_name, auth, params):
+        """
+        Publish to Tableau Server (overwrites existing extract)
+
+        >>> tableau_auth_details = ("usename", "password")
+        >>> publish_details = ("site_content_url", "project_name")
+        >>> tde.publish("tableau server address", auth=tableau_auth_details, params=publish_details)
+        Published!
+        """
+        ts = Tableau()
+        username, password = auth
+        site_content_url, project_name = params
+        ts.connect(host_name, username, password, site_content_url)
+        ts.publish(self.location, project_name)
+        ts.close()
+
+    def cleanup(self):
+        """
+        delete only if temporary
+        :return: 
+        """
+        if not self._is_temp:
+            return
+        os.remove(self.location)
---
+++ trext/extract/server.py
@@ -0,0 +1,33 @@
+"""
+
+connection for tableau
+
+"""
+import re
+from tableausdk.Server import ServerAPI, ServerConnection
+
+
+class Tableau(object):
+    def __init__(self):
+        self.server_init = ServerAPI()
+        self.server_init.initialize()
+        self.server = ServerConnection()
+
+    @staticmethod
+    def _get_extract_name(extract_path):
+        extract_name_regex = re.compile("(\w*).tde$")
+        extract_name = extract_name_regex.findall(extract_path)[0]
+        return extract_name
+
+    def connect(self, host, username, password, site_content_url='Default'):
+        self.server.connect(host, username, password, site_content_url)
+
+    def publish(self, tde_path, project_name='Default', datasource_name=None, overwrite=True):
+        if not datasource_name:
+            datasource_name = self._get_extract_name(tde_path)
+        self.server.publishExtract(tde_path, project_name, datasource_name, overwrite)
+        print "Published!"
+
+    def close(self):
+        self.server.disconnect()
+        self.server_init.cleanup()
