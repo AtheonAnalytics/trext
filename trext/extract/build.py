51d0b59f24c19155c43f5b6ff31e1dbd1bca08ed
Vathsala Achar <vathsala.achar@atheon.co.uk>
2017-05-18 16:21:06 +0100

    docstring for building the extract and db components

--- trext/extract/build.py
+++ trext/extract/build.py
@@ -9,6 +9,11 @@ from trext.db.fill import ExtractFiller
 
 
 class ExtractBuilder(object):
+    """
+    Builds the tableau Extract by creating a Tableau Extract, defines the table skeleton, adds the 
+    table to the extract and fills this table with the relevant data for the TDE. 
+    """
+
     def __init__(self):
         self._db_type = None
         self._db_conn = None
@@ -20,11 +25,17 @@ class ExtractBuilder(object):
         self._tde_path = None
 
     def _build_temp_tdepath(self):
+        """
+        creates a temporary directory and defines the tde path as temp_dir+view_or_table_name.tde
+        """
         temp_dir_path = tempfile.mkdtemp()
         file_name = "{}.tde".format(self._view_or_table_name)
         self._tde_path = os.path.join(temp_dir_path, file_name)
 
-    def _initialize_extract(self):
+    def _initialise_extract(self):
+        """
+        initialises extract with the defined tde path
+        """
         self._extract = Extract(self._tde_path)
 
     def _build_skeleton(self):
@@ -42,27 +53,52 @@ class ExtractBuilder(object):
         return table_def
 
     def _add_table_to_extract(self, table_def):
+        """
+        Assigns the table definition (skeleton) to the extract
+         
+        :param table_def: the skeleton of the view or table
+        :return: updated tableau `Extract`
+        """
         return self._extract.addTable('Extract', table_def)
 
-    def _feed_skeleton(self):
+    def _fill_extract(self):
+        """
+        Fills the Extract with data from the view/table
+        """
         extract_table_definition = self._build_skeleton()
-        tdetable = self._add_table_to_extract(extract_table_definition)
-        extract_feed = ExtractFiller(tdetable, extract_table_definition, self.columns)
+        tde_table = self._add_table_to_extract(extract_table_definition)
+        extract_feed = ExtractFiller(tde_table, extract_table_definition, self.columns)
         for row in self._db_consumer.get_table_data():
             extract_feed.insert_data_to_extract(row)
         self._extract.close()
 
     def connect_to_db(self, view_or_table_name, conn_string, dbtype=None):
+        """
+        Connect to the view or table that needs to be turned into a .tde extract
+        
+        :param view_or_table_name: View or Table that needs to be an extract
+        :param conn_string: connection string to the database where the view or table exists
+        :param dbtype: type of db so the right pyodbc wrapper is used to connect
+        """
         self._db_conn = AnyDB(conn_string, dbtype)
         self._cursor = self._db_conn.get_cursor()
         self._db_type = dbtype
         self._view_or_table_name = view_or_table_name
 
     def create_extract(self):
+        """
+        Creates the extract by 
+        - connecting to the database,
+        - building the tde path
+        - initializing the extract
+        - pulling data from db and filling the extract
+        
+        :return: path to the created .tde extract 
+        """
         self._db_consumer = DBConsumer(self._cursor, self._view_or_table_name, self._db_type)
         self._build_temp_tdepath()
-        self._initialize_extract()
-        self._feed_skeleton()
+        self._initialise_extract()
+        self._fill_extract()
         return self._tde_path
 
     def close(self):
--- trext/extract/utils.py
+++ trext/extract/utils.py
@@ -1,10 +1,16 @@
 import re
 
 
-def get_db_components(dbtable):
+def get_db_components(db_table_or_view):
+    """
+    Splits the db_table into 3 components - db, schema and table/view
+    
+    :param db_table_or_view: of the form [DB].[SCHEMA].[TABLE]
+    :return: tuple of db, schema and table
+    """
     db_component_regex = re.compile('([\[^[][\w\.]+\]?|[\w]+)')
     # Split the table path into its components
-    components = re.findall(db_component_regex, dbtable)
+    components = re.findall(db_component_regex, db_table_or_view)
     if len(components) == 2:
         db = None
         schema, table = components
