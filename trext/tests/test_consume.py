911f7fd7b4454cf8d34238494ac829b2cca29cdc
Vathsala Achar <vathsala.achar@atheon.co.uk>
2017-05-23 10:26:19 +0100

    tests for db consume return type check

---
+++ trext/tests/test_consume.py
@@ -0,0 +1,21 @@
+import types
+import mock
+from unittest import TestCase
+
+from trext.db.consume import DBConsumer
+
+
+class TestDBConsumerDefault(TestCase):
+    @mock.patch('trext.db.consume.get_db_components')
+    def test_get_db_columns_return_type(self, mock_get_dbcomps):
+        mock_get_dbcomps.return_value = ('db', 'schema', 'table')
+        db_consumer = DBConsumer('', '', None)
+        res = db_consumer.get_table_definition()
+        self.assertTrue(isinstance(res, types.GeneratorType))
+
+    @mock.patch('trext.db.consume.get_db_components')
+    def test_get_db_data_return_type(self, mock_get_dbcomps):
+        mock_get_dbcomps.return_value = ('db', 'schema', 'table')
+        db_consumer = DBConsumer('', '', None)
+        res = db_consumer.get_table_data()
+        self.assertTrue(isinstance(res, types.GeneratorType))
